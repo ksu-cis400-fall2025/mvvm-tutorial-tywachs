98953b376daa5dce421b4e93103c1694e5abb92d
Tyler Wachsnicht <tywachs@ksu.edu>
2026-03-31 15:49:05 -0500

    done

---
+++ MvvmExample/ComputerScienceStudentViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmExample
+{
+    public class ComputerScienceStudentViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private Student _student;
+        public String FirstName => _student.FirstName;
+        public String LastName => _student.LastName;
+        public IEnumerable<CourseRecord> CourseRecords => _student.CourseRecords;
+        public double GPA => _student.GPA;
+        public double ComputerScienceGPA
+        {
+            get
+            {
+                var points = 0.0;
+                var hours = 0.0;
+                foreach (var cr in _student.CourseRecords)
+                {
+                    if (cr.CourseName.Contains("CIS"))
+                    {
+                        points += (double)cr.Grade * cr.CreditHours;
+                        hours += cr.CreditHours;
+                    }
+                }
+                return points / hours;
+            }
+        }
+        private void HandleStudentPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(Student.GPA))
+            {
+                PropertyChanged?.Invoke(this, e);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ComputerScienceGPA)));
+            }
+        }
+        public ComputerScienceStudentViewModel(Student student)
+        {
+            _student = student;
+            student.PropertyChanged += HandleStudentPropertyChanged;
+        }
+    }
+}
